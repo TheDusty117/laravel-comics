77651950456de9f8dd3736dd8a4ee40b7a3c301f
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-05 17:34:30 +0200

    FEAT logo in header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
   <title>page-title</title>
-
+  <!-- qui importa bootstrap e il resto scss es. -->
   @vite('resources/js/app.js')
+
 </head>
 <body>
 
@@ -21,6 +21,6 @@
 
 
   @include('partials.footer')
-  
+
 </body>
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="main-header">
   <div class="container">
     <h1>ciao sono un HEADER</h1>
-
+    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
   </div>
 </header>
