197c4687c45111603339f813a93eb2edbb500567
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-05 19:12:42 +0200

    FEAT working on content

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,49 @@
 <header class="main-header">
-  <div class="container">
-    <h1>ciao sono un HEADER</h1>
-    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
-  </div>
+  <section class="header-section">
+      <div class="container pt-3">
+          <div class="row justify-content-between align-items-center">
+            <div class="col-1">
+              <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+            <div class="col-8 p-0">
+              <ul class="d-flex justify-content-between p-0 m-0 text-uppercase fw-bold">
+                <li>
+                  characters
+                </li>
+                <li>
+                  comics
+                </li>
+                <li>
+                  movies
+                </li>
+                <li>
+                  tv
+                </li>
+                <li>
+                  games
+                </li>
+                <li>
+                  collectibles
+                </li>
+                <li>
+                  videos
+                </li>
+                <li>
+                  fans
+                </li>
+                <li>
+                  news
+                </li>
+                <li>
+                  shop
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-1">
+              <p>ricerca</p>
+            </div>
+        </div>
+      </div>
+  </section>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,61 @@
 @extends('layouts.app')
 
 @section('content')
-    
-  <div class="container">
-    <h2>SONO CONTENT</h2>
-  </div>
+
+<section class="hero-section">
+
+</section>
+
+<section class="content-section">
+    <div class="container">
+      <h2>SONO CONTENT</h2>
+      <div class="row">
+        <ul>
+          <li>
+            <img src="" alt="">
+            <h4>action comics</h4>
+          </li>
+          <li>
+            <img src="" alt="">
+            <h4>action comics</h4>
+          </li>
+          <li>
+            <img src="" alt="">
+            <h4>action comics</h4>
+          </li>
+          <li>
+            <img src="" alt="">
+            <h4>action comics</h4>
+          </li>
+          <li>
+            <img src="" alt="">
+          </li>
+          <li>
+            <img src="" alt="">
+          </li>
+          <li>
+            <img src="" alt="">
+          </li>
+          <li>
+            <img src="" alt="">
+          </li>
+          <li>
+            <img src="" alt="">
+          </li>
+          <li>
+            <img src="" alt="">
+          </li>
+          <li>
+            <img src="" alt="">
+          </li>
+          <li>
+            <img src="" alt="">
+          </li>
+        </ul>
+      </div>
+    </div>
+</section>
+
+
 
 @endsection
